a1d6222d6e756ce2bbe3287f6188f8dfad156b24
Isaac Elias <isaac_lucarelli.elias@stud.tu-darmstadt.de>
2023-08-03 20:58:51 +0200

    Testing setScopusData(::Abstract) and getScopusCitingAbstracts

--- test/test_hindex.jl
+++ test/test_hindex.jl
@@ -4,7 +4,14 @@ using .HIndex
 
 ENV["JULIA_DEBUG"] = HIndex
 
+# Searching for an author
 hasse = Author("hasse", "technische universitat darmstadt")
 @time setScopusData!(hasse)
 
-articles_hasse = getAuthoredAbstracts(hasse)
+# Getting his papers
+articles_hasse = getScopusAuthoredAbstracts(hasse)
+@time setScopusData!(articles_hasse[1])
+
+# Getting the paper that cites
+@time citing = getScopusCitingAbstracts(articles_hasse[1])
+@info citing
