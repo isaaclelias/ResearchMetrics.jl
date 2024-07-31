f4ded2c5582de9c1e4ddb8e21e13852c6c4b474a
Isaac Elias <isaac.lelias@gmail.com>
2024-07-31 12:17:07 +0200

    prompt to set Scopus API key

--- src/APIs/ApiKeys.jl
+++ src/APIs/ApiKeys.jl
@@ -1,5 +1,5 @@
-scopus_api_key = nothing
-serapi_api_key = nothing
+scopus_api_key::String = ""
+serapi_api_key::String = ""
 
 function setSerpApiKey(key::AbstractString)
     global serapi_api_key = key
@@ -7,4 +7,17 @@ end
 
 function setScopusKey(key::AbstractString)
     global scopus_api_key = key
+end
+
+function prompt_to_set_scopus_key()
+    if length(scopus_api_key) !== 0
+        @info "Scopus API key is already set." scopus_api_key
+        return nothing
+    end
+
+    @info "Scopus API key is not set. Please insert the key below."
+    print("Scopus API key: ")
+    setSerpApiKey(readline())
+
+    return nothing
 end
